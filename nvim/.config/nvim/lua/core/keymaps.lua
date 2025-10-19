241d89dd7261d903a4079f40cea2966978cc576b
JCHacking <juancruzmencia@gmail.com>
2025-10-19 17:44:40 +0200

    feat(nvim): Change keymaps
    Remove redo undo

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -3,16 +3,7 @@ vim.g.mapleader = " "  -- Set SPACE for leader key
 -- Buffers
 vim.keymap.set("n", "<leader>n", "<cmd>bnext<CR>", { desc = "Go to the next buffer" })
 vim.keymap.set("n", "<leader>p", "<cmd>bprev<CR>", { desc = "Go to the previous buffer" })
-vim.keymap.set("n", "<leader>bq", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
-
--- Undo
-vim.keymap.set("n", "U", "<C-r>", { desc = "Redo Undo" })
-
--- Scroll
-vim.keymap.set("n", "<leader>cl", function() vim.opt.scrolloff = 999 - vim.opt.scrolloff end, { desc = "Lock/Unlock cursor line stay in the middle" })
-
--- Search
-vim.keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>q", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
 
 -- Tabs
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnext<CR>", { desc = "Go to the next tab" })
@@ -28,4 +19,10 @@ vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically"})
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally"})
 vim.keymap.set("n", "<leader>s=", "<C-w>=", { desc = "Resize windows to make equal size"})
 vim.keymap.set("n", "<leader>sq", "<cmd>close<CR>", { desc = "Close current split"})
+
+-- Scroll
+vim.keymap.set("n", "<leader>cl", function() vim.opt.scrolloff = 999 - vim.opt.scrolloff end, { desc = "Lock/Unlock cursor line stay in the middle" })
+
+-- Search
+vim.keymap.set("n", "<leader>nh", "<cmd>nohl<CR>", { desc = "Clear search highlights" })
 
