4278b1e49fff035deefd5f7bdf7fc8c1990d27c9
JCHacking <juancruzmencia@gmail.com>
2025-12-31 13:47:18 +0100

    feat(zsh): Add ssh-nosec alias for avoid known hosts check

--- zsh/config/alias.zsh
+++ zsh/config/alias.zsh
@@ -1,4 +1,9 @@
-# Replace applications
+# Ask confirmation
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+
+# Replace applications with modern alternatives
 alias cat="bat"
 alias diff="delta"
 alias dir="ls"
@@ -6,10 +11,10 @@ alias find="fd"
 alias grep="rg --no-ignore --hidden --pcre2"
 alias ls="lsd --long --git"
 alias tree="ls --tree"
+
+# Replace docker to podman
 alias docker="podman"
 alias docker-compose="podman-compose"
 
-# Ask confirmation
-alias cp="cp -i"
-alias mv="mv -i"
-alias rm="rm -i"
+# Add useful ssh aliases
+alias ssh-nosec="ssh -o 'StricthostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' -o 'LogLevel=ERROR'"
