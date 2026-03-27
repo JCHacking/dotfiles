4c5baf6a780fefd50540f9a0f370ca9efe231e01
jmencianaranjo-ionos <jcmencia@arsys.es>
2026-03-27 11:28:31 +0100

    feat(zsh): Add aliases for citrix

--- zsh/config/alias.zsh
+++ zsh/config/alias.zsh
@@ -12,9 +12,12 @@ alias grep="rg --no-ignore --hidden --pcre2"
 alias ls="lsd --long --git"
 alias tree="ls --tree"
 
-# Alias for task (Because go-task rename binary name)
-alias task="go-task"
+# Alias for citrix
+alias citrix="/opt/Citrix/ICAClient/wfica"
 
-# Add useful ssh aliases
+# Add ssh aliases
 alias ssh-nosec="ssh -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' -o 'LogLevel=ERROR'"
 alias sftp-nosec="sftp -o 'StrictHostKeyChecking=no' -o 'UserKnownHostsFile=/dev/null' -o 'LogLevel=ERROR'"
+
+# Alias for task (Because go-task rename binary name)
+alias task="go-task"
