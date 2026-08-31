a7b52b4742ba3ce6b9916e934888a0be197fa024
JCHacking <juancruzmencia@gmail.com>
2026-08-31 16:05:04 +0200

    fix(nvim): Detect new files

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -14,7 +14,7 @@ return {
         typescriptreact = { "biomejs" },
       }
 
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "FileType" }, {
         callback = function()
           lint.try_lint()
         end,
